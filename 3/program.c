fc024f4da1a4fdea376f640cc8222c5a20035cb1
Shishira <shishiraiyar@gmail.com>
2025-01-09 13:37:19 +0000

    Refactored third program

--- 3/program.c
+++ 3/program.c
@@ -3,6 +3,10 @@
 #include<math.h>
 #include<omp.h>
 
+int min(int a, int b) {
+	return (a < b)? a : b;
+}
+
 int markMultiples(int *isComposite, int startIndex, int step, int endIndex) {
 	int i;
 	for (i = startIndex; i <= endIndex; i += step)
@@ -43,24 +47,24 @@ int segmentedSieve(int upperLimit) {
 	int *nextMultiple = (int *)malloc(sqrtLimit * sizeof(int));
 	
 	int primeCountUpToSqrt = 0;
+	int totalPrimeCount = 0;
 
 	double startTime = omp_get_wtime();
 
-	int totalPrimeCount = 0;
 	for (int i = 2; i <= sqrtLimit; i++) {
 		if (!isComposite[i]) {
-			totalPrimeCount++;
 			primes[primeCountUpToSqrt] = i;
 			nextMultiple[primeCountUpToSqrt] = markMultiples(isComposite, i * i, i, sqrtLimit);
 			primeCountUpToSqrt++;
+			totalPrimeCount++;
 		}
 	}
 	
 	for (int left = sqrtLimit + 1; left <= upperLimit; left += sqrtLimit) {
-		int right = (left + sqrtLimit - 1 < upperLimit) ? left + sqrtLimit - 1 : upperLimit;
+		int right = min(left + sqrtLimit - 1, upperLimit);  
 
-		for (int j = 0; j < primeCountUpToSqrt; j++) {
-			nextMultiple[j] = markMultiples(isComposite, nextMultiple[j], primes[j], right);
+		for (int i = 0; i < primeCountUpToSqrt; i++) {
+			nextMultiple[i] = markMultiples(isComposite, nextMultiple[i], primes[i], right);
 		}
 		for (int i = left; i <= right; i++) {
 			if (!isComposite[i])
@@ -85,25 +89,25 @@ int parallelSegmentedSieve(int upperLimit) {
 	int *nextMultiple = (int *)malloc(sqrtLimit * sizeof(int));
 	
 	int primeCountUpToSqrt = 0;
+	int totalPrimeCount = 0;
 
 	double startTime = omp_get_wtime();
 
-	int totalPrimeCount = 0;
 	for (int i = 2; i <= sqrtLimit; i++) {
 		if (!isComposite[i]) {
-			totalPrimeCount++;
 			primes[primeCountUpToSqrt] = i;
 			nextMultiple[primeCountUpToSqrt] = markMultiples(isComposite, i * i, i, sqrtLimit);
 			primeCountUpToSqrt++;
+			totalPrimeCount++;
 		}
 	}
 	
 	for (int left = sqrtLimit + 1; left <= upperLimit; left += sqrtLimit) {
-		int right = (left + sqrtLimit - 1 < upperLimit) ? left + sqrtLimit - 1 : upperLimit;
+		int right = min(left + sqrtLimit - 1, upperLimit);
 		
 		#pragma omp parallel for
-		for (int j = 0; j < primeCountUpToSqrt; j++) {
-			nextMultiple[j] = markMultiples(isComposite, nextMultiple[j], primes[j], right);
+		for (int i = 0; i < primeCountUpToSqrt; i++) {
+			nextMultiple[i] = markMultiples(isComposite, nextMultiple[i], primes[i], right);
 		}
 
 		#pragma omp parallel for reduction(+:totalPrimeCount)
@@ -125,6 +129,7 @@ int parallelSegmentedSieve(int upperLimit) {
 int main(int argc, char *argv[]){
 	if (argc != 2){
 		printf("Error. Usage: %s upperLimit\n", argv[0]);
+		exit(1);
 	}
 	int upperLimit = atoi(argv[1]);
 	printf("Basic sieve prime count: %d\n", basicSieve(upperLimit));
@@ -132,6 +137,8 @@ int main(int argc, char *argv[]){
 	printf("Segmented sieve prime count: %d\n", segmentedSieve(upperLimit));
 
 	printf("Parallel segmented sieve prime count: %d\n", parallelSegmentedSieve(upperLimit));
+
+	return 0;
 }	
 	
 
