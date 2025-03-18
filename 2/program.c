bc4c91d13a4e1b9af5b05b86e203039a2797003b
Shishira <shishiraiyar@gmail.com>
2025-03-18 06:51:47 +0000

    Bug fix in matrix multiplication

--- 2/program.c
+++ 2/program.c
@@ -34,7 +34,7 @@ int main (int argc, char *argv[]) {
 	for (int i=0; i<size; i++){
 		for (int j=0; j<size; j++){
 			for (int k=0; k<size; k++){
-				C[i * size + k] += A[i * size + j] + B[j * size + k];
+				C[i * size + j] += A[i * size + k] * B[k * size + j];
 			}
 		}
 	}
